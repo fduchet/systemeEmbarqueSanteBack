d71f5d34b099d48efb7ece6ad097170c84c7eba5
timnll <timothee.nouaille@epfedu.fr>
2019-10-13 12:27:49 +0200

    Spring project structure

---
+++ projet/src/main/java/watchProject/Controller/Controller.java
@@ -0,0 +1,13 @@
+package watchProject.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Coucou la fafa";
+    }
+}

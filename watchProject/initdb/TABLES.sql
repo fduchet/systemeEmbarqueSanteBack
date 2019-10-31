5e71b15ed19eaadc7b89dfebffb8d6abd57eaf09
sebcomte <sebcomte@orange.fr>
2019-10-31 11:41:33 +0100

    Ajout des fichiers SQL

---
+++ watchProject/initdb/TABLES.sql
@@ -0,0 +1,98 @@
+drop table users;
+drop table runs;
+drop table calories;
+drop table temperatures;
+drop table heart_rates;
+drop table oxygen_levels;
+drop table speeds;
+drop table distances;
+
+create table users
+(
+    id          bigint auto_increment
+        primary key,
+    first_name  text       null,
+    last_name   text       null,
+    weight        text       null,
+    height        text       null,
+    email_address        text       null,
+    age   text       null
+);
+
+create table runs
+(
+    id  bigint auto_increment
+        primary key,
+    total_distance       text       null,
+    total_time         text       null,
+    average_speed   text       null,
+    average_heart_rate text       null,
+    average_oxygen_level        int        null,
+    description text null,
+    user_id bigint,
+    constraint runs_user_fk foreign key(user_id) references users(id)
+
+);
+
+create table oxygen_levels
+(
+    id   bigint auto_increment
+        primary key,
+    time text null ,
+    value text null ,
+    run_id bigint,
+    constraint oxygen_levels_run_fk foreign key(run_id) references runs(id)
+);
+
+create table speeds
+(
+    id   bigint auto_increment
+        primary key,
+    time text null ,
+    value text null ,
+    run_id bigint,
+    constraint speeds_run_fk foreign key(run_id) references runs(id)
+);
+
+create table calories
+(
+    id   bigint auto_increment
+        primary key,
+    time text null ,
+    value text null ,
+    run_id bigint,
+    constraint calories_run_fk foreign key(run_id) references runs(id)
+);
+
+create table distances
+(
+    id   bigint auto_increment
+        primary key,
+    time text null ,
+    value text null ,
+    run_id bigint,
+    constraint distances_run_fk foreign key(run_id) references runs(id)
+);
+
+create table heart_rates
+(
+    id   bigint auto_increment
+        primary key,
+    time text null ,
+    value text null ,
+    run_id bigint,
+    constraint heart_rates_run_fk foreign key(run_id) references runs(id)
+);
+
+create table temperatures
+(
+    id   bigint auto_increment
+        primary key,
+    time text null ,
+    value text null ,
+    run_id bigint,
+    constraint temperatures_run_fk foreign key(run_id) references runs(id)
+);
+
+
+
